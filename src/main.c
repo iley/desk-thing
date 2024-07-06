5c717881c62db1d15cb12466482af76ff0d5fa7b
Ilya Strukov <ilya@istrukov.com>
2024-07-06 23:03:31 +0100

    Make the basic text demo work

--- src/main.c
+++ src/main.c
@@ -6,16 +6,21 @@
 
 #include "display.h"
 
-void draw() {
+void draw(unsigned int offset) {
   u8g2_ClearBuffer(&u8g2);
-  u8g2_ClearDisplay(&u8g2);
+  // u8g2_ClearDisplay(&u8g2);
 
-  /*
   static char *message = "Hello World!";
   u8g2_DrawStr(&u8g2, 10, 10, message);
   u8g2_SetDrawColor(&u8g2, 1);
-  */
-  u8g2_DrawBox(&u8g2, 0, 0, 256, 64);
+
+  u8g2_SetFont(&u8g2, u8g2_font_spleen8x16_mf);
+  u8g2_FirstPage(&u8g2);
+  do {
+    u8g2_DrawStr(&u8g2, offset, 24, "Hello World!");
+    // u8g2_SetDrawColor(&u8g2, 1);
+    // u8g2_DrawBox(&u8g2, 0, 0, 256, 64);
+  } while (u8g2_NextPage(&u8g2));
 
   u8g2_UpdateDisplay(&u8g2);
 }
@@ -26,10 +31,13 @@ int main() {
   display_init();
   printf("Display initialized\n");
 
-  draw();
+  unsigned int offset = 0;
 
   while(true) {
-    sleep_ms(1000);
+    draw(offset);
+    sleep_ms(10);
+
+    offset = (offset + 1) % 256;
   }
 
   return 0;
