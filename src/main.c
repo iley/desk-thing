403d240c76d10fa0401ee8c8b820b012e77fa393
Ilya Strukov <ilya@istrukov.com>
2024-07-06 23:20:22 +0100

    Tweak display code

--- src/main.c
+++ src/main.c
@@ -7,20 +7,12 @@
 #include "display.h"
 
 void draw(unsigned int offset) {
-  u8g2_ClearBuffer(&u8g2);
-  // u8g2_ClearDisplay(&u8g2);
-
   static char *message = "Hello World!";
-  u8g2_DrawStr(&u8g2, 10, 10, message);
-  u8g2_SetDrawColor(&u8g2, 1);
+
+  u8g2_ClearBuffer(&u8g2);
 
   u8g2_SetFont(&u8g2, u8g2_font_spleen8x16_mf);
-  u8g2_FirstPage(&u8g2);
-  do {
-    u8g2_DrawStr(&u8g2, offset, 24, "Hello World!");
-    // u8g2_SetDrawColor(&u8g2, 1);
-    // u8g2_DrawBox(&u8g2, 0, 0, 256, 64);
-  } while (u8g2_NextPage(&u8g2));
+  u8g2_DrawStr(&u8g2, offset, 32, "Hello World!");
 
   u8g2_UpdateDisplay(&u8g2);
 }
@@ -35,8 +27,6 @@ int main() {
 
   while(true) {
     draw(offset);
-    sleep_ms(10);
-
     offset = (offset + 1) % 256;
   }
 
