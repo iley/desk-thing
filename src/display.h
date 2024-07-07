e088018b209d841b5cc253a7f3fd5f295729d128
Ilya Strukov <ilya@istrukov.com>
2024-07-07 10:55:29 +0100

    Tune SPI speed

--- src/display.h
+++ src/display.h
@@ -4,13 +4,14 @@
 #include <u8g2.h>
 
 #define SPI_PORT spi0
-#define SPI_SPEED (4000*1000)
+// According to the datasheet, SSD1322's maximum SPI speed is 10 MHz.
+#define SPI_SPEED (10 * 1e6)
 
-#define PIN_SCK   2
-#define PIN_MOSI  3
-#define PIN_DC    4
-#define PIN_RST   11
-#define PIN_CS    5
+#define PIN_SCK 2
+#define PIN_MOSI 3
+#define PIN_DC 4
+#define PIN_RST 11
+#define PIN_CS 5
 
 extern u8g2_t u8g2;
 
