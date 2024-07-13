bb4d331a0f0d430232216504cd9f571433122592
Ilya Strukov <ilya@istrukov.com>
2024-07-13 19:21:46 +0100

    Board design

--- src/main.c
+++ src/main.c
@@ -12,8 +12,9 @@ void draw(unsigned int offset) {
 
   u8g2_ClearBuffer(&u8g2);
 
-  u8g2_SetFont(&u8g2, u8g2_font_spleen8x16_mf);
-  u8g2_DrawStr(&u8g2, offset, 32, "Hello World!");
+  // u8g2_SetFont(&u8g2, u8g2_font_spleen8x16_mf);
+  // u8g2_DrawStr(&u8g2, offset, 32, "Hello World!");
+  u8g2_DrawBox(&u8g2, 0, 0, offset, 64);
 
   u8g2_UpdateDisplay(&u8g2);
 }
@@ -26,7 +27,7 @@ int main() {
 
   unsigned int offset = 0;
 
-  while(true) {
+  while (true) {
     draw(offset);
     offset = (offset + 1) % 256;
   }
