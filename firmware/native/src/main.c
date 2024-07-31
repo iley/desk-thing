c86c02b4f293e9d17d26a10f23f5a8d2e18006b2
Ilya Strukov <ilya@istrukov.com>
2024-07-31 20:41:54 +0100

    PCB v1.1

--- firmware/native/src/main.c
+++ firmware/native/src/main.c
@@ -7,29 +7,32 @@
 #include "display.h"
 #include "sleep.h"
 
-void draw(unsigned int offset) {
+#define ROWS 8
+#define ROW_WIDTH 8
+
+void draw() {
   static char *message = "Hello World!";
+  static unsigned int offsets[ROWS] = {0};
 
   u8g2_ClearBuffer(&u8g2);
 
-  u8g2_SetFont(&u8g2, u8g2_font_spleen8x16_mf);
-  u8g2_DrawStr(&u8g2, offset, 32, "Hello World!");
-  // u8g2_DrawBox(&u8g2, 0, 0, offset, 64);
+  // u8g2_SetFont(&u8g2, u8g2_font_spleen8x16_mf);
+  u8g2_SetFont(&u8g2, u8g2_font_amstrad_cpc_extended_8f);
+  for (int i = 0; i < ROWS; i++) {
+    u8g2_DrawStr(&u8g2, offsets[i], ROW_WIDTH * (i + 1), "Hello World!");
+    offsets[i] = (offsets[i] + i % 3 + 1) % 256;
+  }
 
-  u8g2_UpdateDisplay(&u8g2);
+  u8g2_SendBuffer(&u8g2);
 }
 
 int main() {
   stdio_init_all();
 
   display_init();
-  printf("Display initialized\n");
-
-  unsigned int offset = 0;
 
   while (true) {
-    draw(offset);
-    offset = (offset + 1) % 256;
+    draw();
   }
 
   return 0;
